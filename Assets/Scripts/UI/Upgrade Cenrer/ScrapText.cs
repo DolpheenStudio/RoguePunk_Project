6c691cce491520515b68124b8278aee6905f285e
DolpheenStudio <roguepunk.project@gmail.com>
2022-03-11 19:45:48 +0100

    Fix

--- Assets/Scripts/UI/Upgrade Cenrer/ScrapText.cs
+++ Assets/Scripts/UI/Upgrade Cenrer/ScrapText.cs
@@ -15,6 +15,6 @@ public class ScrapText : MonoBehaviour
 
     void Update()
     {
-        scrapInfo.text = "Scraps: " + player.playerScraps + " " + Cursor.visible;
+        scrapInfo.text = "Scraps: " + player.playerScraps;
     }
 }
