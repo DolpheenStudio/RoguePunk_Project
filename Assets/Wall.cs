b305448496cc881cfc047636e8f96ccf962292b2
DolpheenStudio <roguepunk.project@gmail.com>
2022-04-05 01:43:17 +0200

    Floor Model
    Added:
    - 3 floor models
    - Random floor generation

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -9,22 +9,23 @@ public class Wall : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<Player>();
+        position = 0f;
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         if(Vector3.Distance(new Vector3(player.transform.position.x, 0f, player.transform.position.z), 
                             new Vector3(transform.position.x, 0f, transform.position.z)) <= 15 &&
-                            (player.transform.position.x < transform.position.x - 3 || player.transform.position.z < transform.position.z - 3))
+                            ((player.transform.position.x + player.transform.position.z) <= (transform.position.x + transform.position.z)))
         {
             transform.position = new Vector3(transform.position.x, position, transform.position.z);
-            if(position >= -4) position -= Time.deltaTime * 20f;
+            if (position >= -4) position -= Time.deltaTime * 20f;
         }
         else
         {
             transform.position = new Vector3(transform.position.x, position, transform.position.z);
-            if(position <= 0) position += Time.deltaTime * 20f;
+            if(position <= -0.1) position += Time.deltaTime * 20f;
         }
     }
 }
